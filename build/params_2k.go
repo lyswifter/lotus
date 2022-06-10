cb4a502093dfaf0cf4706241ce105122014441e0
vyzo <vyzo@hackzen.org>
2022-06-10 14:47:19 +0300

    use filecoin-ffi@feat/debug-execution
    implement dual execution
    
    update filecoin-ffi
    
    disable skyr upgrade in 2k net
    
    fix bogus error message
    
    update filecoin-ffi
    
    update filecoin-ffi
    
    update filecoin-ffi
    
    pass the actor redirect mapping through the overlay blockstore
    
    update filecoin-ffi
    
    update filecoin-ffi
    
    go mod stuffs
    
    make yushie compatible
    
    refactor utilities
    
    fix lint

--- build/params_2k.go
+++ build/params_2k.go
@@ -7,8 +7,6 @@ import (
 	"os"
 	"strconv"
 
-	"github.com/ipfs/go-cid"
-
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/network"
 
--- chain/vm/vmi.go
+++ chain/vm/vmi.go
@@ -3,10 +3,10 @@ package vm
 import (
 	"context"
 	"os"
-
-	"github.com/ipfs/go-cid"
+	"sync"
 
 	"github.com/filecoin-project/go-state-types/network"
+	cid "github.com/ipfs/go-cid"
 
 	"github.com/filecoin-project/lotus/chain/types"
 )
@@ -25,13 +25,89 @@ var useFvmForMainnetV15 = os.Getenv("LOTUS_USE_FVM_TO_SYNC_MAINNET_V15") == "1"
 
 func NewVM(ctx context.Context, opts *VMOpts) (Interface, error) {
 	if opts.NetworkVersion >= network.Version16 {
+		if os.Getenv("LOTUS_FVM_DEBUG") == "1" {
+			return NewDualExecutionVM(ctx, opts)
+		}
 		return NewFVM(ctx, opts)
 	}
 
 	// Remove after v16 upgrade, this is only to support testing and validation of the FVM
 	if useFvmForMainnetV15 && opts.NetworkVersion >= network.Version15 {
+		if os.Getenv("LOTUS_FVM_DEBUG") == "1" {
+			return NewDualExecutionVM(ctx, opts)
+		}
 		return NewFVM(ctx, opts)
 	}
 
 	return NewLegacyVM(ctx, opts)
+}
+
+type dualExecutionVM struct {
+	main  *FVM
+	debug *FVM
+}
+
+var _ Interface = (*dualExecutionVM)(nil)
+
+func NewDualExecutionVM(ctx context.Context, opts *VMOpts) (Interface, error) {
+	main, err := NewFVM(ctx, opts)
+	if err != nil {
+		return nil, err
+	}
+
+	debug, err := NewDebugFVM(ctx, opts)
+	if err != nil {
+		return nil, err
+	}
+
+	return &dualExecutionVM{
+		main:  main,
+		debug: debug,
+	}, nil
+}
+
+func (vm *dualExecutionVM) ApplyMessage(ctx context.Context, cmsg types.ChainMsg) (ret *ApplyRet, err error) {
+	var wg sync.WaitGroup
+
+	wg.Add(2)
+
+	go func() {
+		defer wg.Done()
+		ret, err = vm.main.ApplyMessage(ctx, cmsg)
+	}()
+
+	go func() {
+		defer wg.Done()
+		if _, err := vm.debug.ApplyMessage(ctx, cmsg); err != nil {
+			log.Errorf("debug execution failed: %w", err)
+		}
+	}()
+
+	wg.Wait()
+	return ret, err
+}
+
+func (vm *dualExecutionVM) ApplyImplicitMessage(ctx context.Context, msg *types.Message) (ret *ApplyRet, err error) {
+	var wg sync.WaitGroup
+
+	wg.Add(2)
+
+	go func() {
+		defer wg.Done()
+		ret, err = vm.main.ApplyImplicitMessage(ctx, msg)
+	}()
+
+	go func() {
+		defer wg.Done()
+		if _, err := vm.debug.ApplyImplicitMessage(ctx, msg); err != nil {
+			log.Errorf("debug execution failed: %w", err)
+		}
+	}()
+
+	wg.Wait()
+	return ret, err
+}
+
+func (vm *dualExecutionVM) Flush(ctx context.Context) (cid.Cid, error) {
+	return vm.main.Flush(ctx)
 }
